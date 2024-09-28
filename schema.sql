f0770dfe56edba765336aca73d8d452c8dab874b
@brno.ov <145804712+BorenoBala@users.noreply.github.com>
2024-09-28 12:01:25 -0300

    alteração e reformulação.

--- schema.sql
+++ schema.sql
@@ -1,53 +1,50 @@
 CREATE TABLE equipe (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR (100) not null,
-    escudo VARCHAR (255) not null
-
+    nome VARCHAR(100) NOT NULL,
+    escudo VARCHAR(255) NOT NULL
 );
 
 
 CREATE TABLE usuario (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) not null,
-    email VARCHAR(100) not null,
-    senha VARCHAR(100) not null,
-    cpf VARCHAR (20) not null,
-    endereco VARCHAR(100) not null,
-    data_nascimento date not null,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    senha VARCHAR(100) NOT NULL,
+    cpf VARCHAR(20) NOT NULL,
+    endereco VARCHAR(100) NOT NULL UNIQUE,  
+    data_nascimento DATE NOT NULL,
     time_id INT,
-    FOREIGN KEY (time_id) references equipe(id)
-    
+    FOREIGN KEY (time_id) REFERENCES equipe(id)
 );
 
 CREATE TABLE endereco (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    logradouro VARCHAR(100) not null,
-    rua VARCHAR(100) not null,
-    numero VARCHAR(10) not null,
-    bairro VARCHAR(100) not null,
-    cidade VARCHAR(100) not null,
-    estado VARCHAR(50) not null,
-    cep VARCHAR(10) not null,
-    enderecoID INT,
-    FOREIGN KEY (enderecoID) references usuario(endereco)
+    logradouro VARCHAR(100) NOT NULL,
+    rua VARCHAR(100) NOT NULL,
+    numero VARCHAR(10) NOT NULL,
+    bairro VARCHAR(100) NOT NULL,
+    cidade VARCHAR(100) NOT NULL,
+    estado VARCHAR(50) NOT NULL,
+    cep VARCHAR(10) NOT NULL,
+    endereco VARCHAR(100),
+    FOREIGN KEY (endereco) REFERENCES usuario(endereco)
 );
 
 CREATE TABLE agendamento (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    data_agendamento date not null,
-    local_agendamento VARCHAR(100) not null,
-    horario time not null,
-    agendamentoID INT,
-    FOREIGN KEY (agendamentoID) references usuario(id)
+    data_agendamento DATE NOT NULL,
+    local_agendamento VARCHAR(100) NOT NULL,
+    horario TIME NOT NULL,
+    usuario_id INT,
+    FOREIGN KEY (usuario_id) REFERENCES usuario(id)
 );
 
 CREATE TABLE estatisticas(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    gols numeric (100),
-    assistencias numeric (100),
-    cartao_amarelo numeric (100),
-    cartao_vermelho numeric (100),
-    estatisticasID INT,
-    FOREIGN KEY (estatisticasID) references usuario(id)
-
+    gols INT,
+    assistencias INT,
+    cartao_amarelo INT,
+    cartao_vermelho INT,
+    usuario_id INT,
+    FOREIGN KEY (usuario_id) REFERENCES usuario(id)
 );
